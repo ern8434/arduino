4e1c5f8c2cce62aeb0d73bb76710d89c4a9b1024
Eren <erenezgu@gmail.com>
2025-02-03 13:03:25 +0300

    ..

--- robot_rover/robot_rover.ino
+++ robot_rover/robot_rover.ino
@@ -13,12 +13,13 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   
-  //pinMode(ENA, OUTPUT);
-  //pinMode(ENB, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 }
  
 void loop() {
-  analogWrite(ENA, 200);     // Hız ayarı yapılıyor (0-255 arası yazılabilir)
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
   
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
@@ -28,13 +29,14 @@ void loop() {
   digitalWrite(IN2, LOW);
   delay(400);              // Motorun durduğu süre. Hızlı dönüşlerden sonra
   
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
   delay(2000);
 
-  digitalWrite(IN1, LOW);   // IN1 ve IN2'yi LOW yaptığımızda motor durur
-  digitalWrite(IN2, LOW);
-  delay(400);              // Motorun durduğu süre. Hızlı dönüşlerden sonra
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  
+  delay(1000);              // Motorun durduğu süre. Hızlı dönüşlerden sonra
                             // yön değişimi yapacaksak bir süre beklemek
                             // sürücü kart ve motoru zorlamamak için faydalıdır
  /*
