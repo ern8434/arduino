de29be666f5b3662e67e5841fc4206696f9f078a
Eren <erenezgu@gmail.com>
2025-02-06 13:24:25 +0300

    ...

--- robot_rover/robot_rover.ino
+++ robot_rover/robot_rover.ino
@@ -1,11 +1,11 @@
 
-int IN1 = 2;    // Sürücü kartın IN1 bağlantısı
-int IN2 = 3;    // Sürücü kartın IN2 bağlantısı
-int IN3 = 4;    // Sürücü kartın IN3 bağlantısı
-int IN4 = 5;    // Sürücü kartın IN4 bağlantısı
+int IN1 = 5;    // Sürücü kartın IN1 bağlantısı
+int IN2 = 4;    // Sürücü kartın IN2 bağlantısı
+int IN3 = 3;    // Sürücü kartın IN3 bağlantısı
+int IN4 = 2;    // Sürücü kartın IN4 bağlantısı
 
-int ENA = 9;
-int ENB = 10;   
+int ENA = 10;
+int ENB = 9;   
 
 void setup() {
   pinMode(IN1, OUTPUT);
@@ -13,28 +13,37 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   
-  //pinMode(ENA, OUTPUT);
-  //pinMode(ENB, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 }
  
 void loop() {
-  analogWrite(ENA, 130);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
   
   
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
+  digitalWrite(IN4, HIGH);
+  digitalWrite(IN3, LOW);
   delay(2000);
   
-  digitalWrite(IN1, LOW);   // IN1 ve IN2'yi LOW yaptığımızda motor durur
+  digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN3, LOW);
   delay(1000);
 
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN3, HIGH);
   delay(2000);
   
-  digitalWrite(IN1, LOW);   // IN1 ve IN2'yi LOW yaptığımızda motor durur
+  digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN3, LOW);
   delay(1000);
 
 }
