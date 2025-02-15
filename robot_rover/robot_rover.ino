510078b3f9d316877862909eae853acf6041528b
Eren <erenezgu@gmail.com>
2025-02-15 14:26:13 +0300

    ..

--- robot_rover/robot_rover.ino
+++ robot_rover/robot_rover.ino
@@ -1,11 +1,53 @@
 
-int IN1 = 5;    // Sürücü kartın IN1 bağlantısı
-int IN2 = 4;    // Sürücü kartın IN2 bağlantısı
-int IN3 = 3;    // Sürücü kartın IN3 bağlantısı
-int IN4 = 2;    // Sürücü kartın IN4 bağlantısı
+int IN1 = 5;
+int IN2 = 4;
+int IN3 = 3;
+int IN4 = 2;
 
 int ENA = 10;
-int ENB = 9;   
+int ENB = 9;
+
+int yon = 1; 
+
+// 1:ileri -1:geri 0:dur
+
+void yon_sec(int y){ 
+  // Eğer ters yön seçildiyse durup bir süre bekleme yap
+  if(y!=0 && y!=yon){
+    dur();
+    delay(500);
+  }
+  if(y==1){
+    yon_ileri();
+  }
+  else if(y==-1){
+    yon_geri();
+  }
+  else {
+    dur();
+  }
+
+  yon = y;// yeni yön değerini sakla
+}
+
+void dur(){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+void yon_ileri(){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void yon_geri(){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
 
 void setup() {
   pinMode(IN1, OUTPUT);
@@ -13,37 +55,23 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
+  //pinMode(ENA, OUTPUT);
+  //pinMode(ENB, OUTPUT);
 }
  
 void loop() {
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 200);
+  int EN = 150;
+  
+  analogWrite(ENA, EN);
+  analogWrite(ENB, EN);
   
   
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN4, HIGH);
-  digitalWrite(IN3, LOW);
-  delay(2000);
-  
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN3, LOW);
-  delay(1000);
+  yon_ileri(); delay(1000);
+  dur();
 
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN3, HIGH);
+  yon_geri(); delay(1000);
+  dur();
+
   delay(2000);
-  
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN3, LOW);
-  delay(1000);
 
 }
