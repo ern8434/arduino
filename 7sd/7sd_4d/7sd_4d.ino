3d593be3a52fb83e7fd21e066d67559be3848e34
Eren <erenezgu@gmail.com>
2025-02-17 18:47:44 +0300

    7sd_4d düğme kullanımı start/reset

--- 7sd/7sd_4d/7sd_4d.ino
+++ 7sd/7sd_4d/7sd_4d.ino
@@ -3,23 +3,59 @@
 int CLK = 2;  // Modülün CLK pini
 int DIO = 3;  // Modülün DIO pini
 
+int dugme_baslat = 9;
+int dugme_reset = 8;
+
+int baslat = 0;
+int reset = 0;
+
+int zaman = 0;
+int aktif = 1; // 0 ise bekleyecek
+
+
 TM1637Display display(CLK, DIO);  // Kütüphane tanımlanıyor
 
 
 void setup() {
   // put your setup code here, to run once:
   display.setBrightness(2);  // Parlaklık ayarı (0 en düşük, 7 en yüksek)
+
+  pinMode(9,INPUT);
+  pinMode(8,INPUT);
+}
+
+void zaman_goster(){
+  int dakika = floor(zaman/60);
+  int saniye = zaman%60;
+  display.showNumberDecEx(100*dakika + saniye,0x40,true);
 }
 
 void loop() {
-  int saniye = 0;
-  int dakika = 0;
+  // baslatma düğmesine basılırsa başla
+  baslat = digitalRead(dugme_baslat);
+  reset = digitalRead(dugme_reset);
+  
+  int delaytime = 1000;
 
-  for (int x = 1; x < 3600; x++) {
-    dakika = floor(x/60);
-    saniye = x%60;
 
-    display.showNumberDecEx(100*dakika + saniye,0x40,true);
-    delay(1000);
+  if(baslat){
+    if(aktif==1){
+      aktif=0;
+    }
+    else if(aktif==0){
+      aktif=1;
+    }
+    delay(200);
+    delaytime -= 200;
   }
+  
+  if(aktif){ // beklemede değilse zamanı ilerlet
+    zaman++;
+  }
+  else if(reset){ // durmuş haldeyken reset basıldıysa sıfırla
+    zaman = 0;
+  }
+  zaman_goster();
+
+  delay(delaytime);
 }
